fc30200c5b9d9200caee60633197e1a2ce4e59f1
AnyaChickenMcnuggets <brat.illidana@gmail.com>
2023-04-08 20:38:17 +0300

    отключен бикрипт

--- src/main/java/com/example/finalfullstack/config/SecurityConfig.java
+++ src/main/java/com/example/finalfullstack/config/SecurityConfig.java
@@ -6,14 +6,20 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig{
 
     private final PersonDetailsService personDetailsService;
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
