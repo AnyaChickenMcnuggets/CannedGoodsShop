04c7ba1aa95a1820dda458e65a021ec02c9fc98f
AnyaChickenMcnuggets <brat.illidana@gmail.com>
2023-04-08 19:22:23 +0300

    Создание проекта

---
+++ src/main/java/com/example/finalfullstack/FinalFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalFullStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalFullStackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/finalfullstack/FinalFullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.finalfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalFullStackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
