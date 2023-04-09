440ac17facf873d3b84c5819ef5c78185f88444f
AnyaChickenMcnuggets <brat.illidana@gmail.com>
2023-04-09 11:05:32 +0300

    бд продуктов поиск, фильтр, сортировка на общей странице товаров

--- src/main/java/com/example/finalfullstack/repositories/ProductRepository.java
+++ src/main/java/com/example/finalfullstack/repositories/ProductRepository.java
@@ -2,9 +2,28 @@ package com.example.finalfullstack.repositories;
 
 import com.example.finalfullstack.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
+    List<Product> findByTitleContainingIgnoreCase(String name);
+
+    @Query(value = "select * from product where ((lower(title) LIKE %?1%) or (lower(title) LIKE '?1%') or (lower(title) like '%?1')) and (price >= ?2 and price <= ?3)", nativeQuery = true)
+    List<Product> findByTitleAndPriceGreaterThanEqualAndPriceLessThanEqual(String title, float ot, float dO);
+
+    @Query(value = "select * from product where ((lower(title) LIKE %?1%) or (lower(title) LIKE '?1%') or (lower(title) like '%?1')) and (price >= ?2 and price <= ?3) order by price)", nativeQuery = true)
+    List<Product> findByTitleOrderByPriceAsc(String title, float ot, float dO);
+
+    @Query(value = "select * from product where ((lower(title) LIKE %?1%) or (lower(title) LIKE '?1%') or (lower(title) like '%?1')) and (price >= ?2 and price <= ?3) order by price desc)", nativeQuery = true)
+    List<Product> findByTitleOrderByPriceDesc(String title, float ot, float dO);
+
+    @Query(value = "select * from product where category_id = ?4 and ((lower(title) LIKE %?1%) or (lower(title) LIKE '?1%') or (lower(title) like '%?1')) and (price >= ?2 and price <= ?3) order by price)", nativeQuery = true)
+    List<Product> findByTitleAndCategoryOrderByPriceAsc(String title, float ot, float dO, int category);
+
+    @Query(value = "select * from product where category_id = ?4 and ((lower(title) LIKE %?1%) or (lower(title) LIKE '?1%') or (lower(title) like '%?1')) and (price >= ?2 and price <= ?3) order by price desc)", nativeQuery = true)
+    List<Product> findByTitleAndCategoryOrderByPriceDesc(String title, float ot, float dO, int category);
 }
